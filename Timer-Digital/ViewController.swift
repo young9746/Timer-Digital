9ca4ac9f91d7d4f828de45c8ee64f4877d948b62
D7703_30 <d7703_30@d7703-30ui-imac.local>
2018-04-11 12:29:14 +0900

    Timer-Digital

--- Timer-Digital/ViewController.swift
+++ Timer-Digital/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // timerSwitch는 한개는 아울렛 한개는 액션
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var timerSwitch: UISwitch!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /*
+         myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+         */
+        timerSwitch.setOn(false, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /* @objc */
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        formatter.dateFormat = "HH:mm:ss EE YYYY"
+        timeLabel.text = formatter.string(from: date)
     }
-
-
+    
+    // 두번째 사용 스위치 액션으로 두기
+    @IBAction func timerStarter(_ sender: Any) {
+        // 클로저(closure)의 사용
+        if timerSwitch.isOn == true {
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()})
+        } else {
+            myTimer.invalidate()
+        }
+    }
 }
-
